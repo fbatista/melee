275beec8b5d0110fe1fffd9f46694500d3f88bb0
Fábio Batista <fbatista@webreakstuff.com>
2011-07-29 16:08:33 +0100

    new idea id generate

--- lib/server.rb
+++ lib/server.rb
@@ -8,25 +8,33 @@ get "/" do
 end
 
 def get_ideas(id)
-	ideas = $redis.smembers "session:#{id}:ideas"
+	ideas = $redis.zrange "session:#{id}:ideas", 0, -1
 	ideas.map do |idea|
 		$redis.hgetall(idea)
 	end
 end
 
 def destroy_idea(session, idea)
-	$redis.srem "session:#{session}:ideas", "idea:#{session}:#{idea}"
+	$redis.zrem "session:#{session}:ideas", "idea:#{session}:#{idea}"
 	$redis.del "idea:#{session}:#{idea}"
 end
 
 def save_idea(session, idea)
-	# TODO atomicity between scard and hset
-	idea[:id] = $redis.scard("session:#{session}:ideas").to_i + 1
+	# TODO atomicity
+	idea[:id] = get_new_id("session:#{session}:ideas")
 	idea.each do |k,v|
 		$redis.hset "idea:#{session}:#{idea[:id]}", k, v
 	end
-	$redis.sadd "session:#{session}:ideas", "idea:#{session}:#{idea[:id]}"
+	$redis.zadd "session:#{session}:ideas", 0, "idea:#{session}:#{idea[:id]}"
 	idea
+end
+
+def get_new_id(set_id, sorted=true)
+	candidate = ""
+	begin
+		candidate = (rand * 9999999999999999 + Time.now.to_i).to_i.to_s(36) << (rand * 9999999999999999 + Time.now.to_i).to_i.to_s(36) << (rand * 9999999999999999 + Time.now.to_i).to_i.to_s(36)
+	end while (sorted ? !!$redis.zrank(set_id, candidate) : $redis.sismember(set_id, candidate))
+	return candidate
 end
 
 post "/:id/ideas" do
@@ -47,11 +55,11 @@ end
 
 # Session urls
 get "/:id" do
-	erb "window.ideas = new IdeaList(#{get_ideas(params[:id]).to_json}, {url:'/#{params[:id]}/ideas'});"
+	redirect_to "/#{params[:id]}/ideate"
 end
 
 get "/:id/ideate" do
-	erb "IDEATE ROUTE"
+	erb "window.ideas = new IdeaList(#{get_ideas(params[:id]).to_json}, {url:'/#{params[:id]}/ideas'});"
 end
 
 get "/:id/cluster" do
